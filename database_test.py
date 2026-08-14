41741857ed86244a13957828e51030989cecc36a
NsovoNkunaSA <nsovo.ike03@gmail.com>
2026-08-14 18:06:37 +0200

    Add MySQL connection test

---
+++ database_test.py
@@ -0,0 +1,12 @@
+import mysql.connector
+
+connection = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="password",
+    database="football_analysis"
+)
+
+print("Connected to MySQL successfully!")
+
+connection.close()
